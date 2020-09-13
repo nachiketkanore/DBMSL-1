4ff5a4fcb9e1ba035d1156ad316cbe29061ac867
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-09-14 01:15:28 +0530

    Update A5.sql

--- A5.sql
+++ A5.sql
@@ -1,3 +1,100 @@
+SHOW databases;
+use DBMSL; 
+
+show tables;
+
+-- Creating customer table 
+CREATE TABLE customer (
+	customer_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    payment_date DATE NOT NULL,
+	scheme_name VARCHAR(20) NOT NULL,
+    status VARCHAR(2) NOT NULL,
+    PRIMARY KEY(customer_id)
+);
+-- YEAR - MONTH - DAY
+INSERT into customer (name, payment_date, scheme_name, status)
+VALUES ('abhishek', '20-FEB-20', 'ABC', 'N');
+
+
+-- Creating fine table
+CREATE TABLE fine (
+	customer_id INT NOT NULL,
+    fine_date DATE NOT NULL,
+    amount INT NOT NULL
+);
+
+DELIMITER $$
+CREATE PROCEDURE check_fine(
+	IN cid int
+)
+BEGIN
+	DECLARE namesch VARCHAR(10);
+	DECLARE dateop date;
+	DECLARE amount real;
+	DECLARE currdate date DEFAULT '2020-09-1';
+	DECLARE days int;
+	DECLARE val int;
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SHOW ERRORS;  -- this is the only one which you need
+        ROLLBACK;   
+    END; 
+
+	select COUNT(*) into val from customer where customer_id = cid;
+	if (val = 0) then
+		set val := 0;
+	end if;
+  
+	select payment_date into dateop from customer where customer_id = cid;  
+	set days := DATEDIFF(currdate, dateop);
+  
+	if (days>15 and days<30) then
+		set amount := days*5;
+		insert into fine values(cid,currdate,amount);
+		update customer set status = 'P' where customer_id = cid;
+	end if;
+  
+	if (days>30) then
+		set amount := 150+((days-30)*50);  
+		insert into fine values(cid,currdate,amount);
+		update customer set status = 'P' where customer_id = cid;
+	end if;
+    
+END$$ 
+DELIMITER ;   
+
+call check_fine(1);
+drop procedure check_fine;
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 CREATE OR REPLACE PROCEDURE check_fine(cid in int) is
 
   namesch varchar(20);
